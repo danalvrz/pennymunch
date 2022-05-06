3280c85be9b20a6a8f1930e03ddaa174fd8c2506
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-05 19:05:57 -0500

    Deploy fix

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,6 +1,6 @@
 class Cost < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :category
+  belongs_to :category
   validates :name, presence: true, length: { minimum: 3 }
   validates :amount, presence: true, length: { minimum: 2 }
 end
